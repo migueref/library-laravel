475cbeb60aa87cb613aa1311de1520f28d5b3862
migueref <reafmiguel@gmail.com>
2018-02-22 23:27:12 -0600

    Authentication scaffolding

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ Route::get('/', function () {
 Route::resource('books', 'Books\BooksController');
 Route::resource('authors', 'Authors\AuthorsController');
 Route::resource('categories', 'Categories\CategoriesController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
