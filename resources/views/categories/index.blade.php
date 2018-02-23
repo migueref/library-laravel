b7e5ff968e5f4f2f9bacd860f78f6f24239092f4
migueref <reafmiguel@gmail.com>
2018-02-22 23:51:53 -0600

    App layout

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,12 +1,14 @@
-<div class='row'>
-  @foreach($categories as $categoria)
-  <div class="card" class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-    <div class="card-body">
-      <h5 class="card-title">{{$categoria->name}}</h5>
-      <p class="card-text">{{$categoria->description}}</p>
-      
-
-    </div>
+@extends('layouts.app')
+@section("title","Lista de categorias")
+@section("content")
+  <div class='row'>
+    @foreach($categories as $categoria)
+      <div class="card mr-3 col-xs-12 col-sm-6 col-md-4 col-lg-3">
+        <div class="card-body">
+          <h5 class="card-title">{{$categoria->name}}</h5>
+          <p class="card-text">{{$categoria->description}}</p>
+        </div>
+      </div>
+    @endforeach
   </div>
-  @endforeach
-</div>
+@endsection
